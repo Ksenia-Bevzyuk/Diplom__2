d8c2317c6cc83fc7bd9ffc565713fa0f86a193a9
Ksenia Bevzyuk <ksunechka-92@yandex.ru>
2025-03-15 22:45:22 +0300

    add modif src/test/java/LoginUserTest.java

--- src/test/java/LoginUserTest.java
+++ src/test/java/LoginUserTest.java
@@ -9,7 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeTrue;
 
 public class LoginUserTest {
@@ -18,12 +17,18 @@ public class LoginUserTest {
     private Credentials credentials;
     private ValidatableResponse response;
     private String genEmail;
+    private String genPass;
+    private String genName;
 
     @Before
+    @DisplayName("Перед каждым тестом")
+    @Description("Генерация данных для создания пользователя, создание пользователя")
     public void before() {
         genEmail = User.generationEmail();
+        genPass = User.generationPass();
+        genName = User.generationName();
 
-        user = new User(genEmail, "password", "Павел");
+        user = new User(genEmail, genPass, genName);
         client = new StellarBurgersClient("");
         response = client.createUser(user);
         assumeTrue(response.extract().statusCode() == SC_OK);
@@ -40,67 +45,60 @@ public class LoginUserTest {
     }
 
     @Test
-    @DisplayName("POST /api/auth/login попытка входа под несуществующим пользователем с некорректным email")
-    @Description("Код 401 при передаче в body несуществующего email и корректного password")
+    @DisplayName("POST /api/auth/login попытка входа под несуществующим пользователем" +
+            " с некорректным email")
+    @Description("Код 401 при передаче в body несуществующего email и" +
+            " корректного password")
     public void userLoginWithIncorrectEmailAndCorrectPass401Test() {
-        credentials = new Credentials(genEmail + "q", "password");
+        credentials = new Credentials(genEmail + "q", genPass);
         response = client.loginUser(credentials);
 
-        int statusCode = response.extract().statusCode();
-        boolean success = response.extract().jsonPath().getBoolean("success");
-        String message = response.extract().jsonPath().getString("message");
-        assertEquals("Ожидается код ответа 401", SC_UNAUTHORIZED, statusCode);
-        assertEquals("Ожидается false", false, success);
-        assertEquals("Ожидается другое сообщение",
-                "email or password are incorrect", message);
+        response.assertThat()
+                .statusCode(SC_UNAUTHORIZED)
+                .body("success", equalTo(false))
+                .body("message", equalTo("email or password are incorrect"));
     }
 
     @Test
-    @DisplayName("POST /api/auth/login попытка входа под несуществующим пользователем с некорректным password")
+    @DisplayName("POST /api/auth/login попытка входа под несуществующим пользователем" +
+            " с некорректным password")
     @Description("Код 401 при передаче в body существующего email и некорректного password")
     public void userLoginWithCorrectEmailAndIncorrectPass401Test() {
-        credentials = new Credentials(genEmail, "pass");
+        credentials = new Credentials(genEmail, genPass + "q");
         response = client.loginUser(credentials);
 
-        int statusCode = response.extract().statusCode();
-        boolean success = response.extract().jsonPath().getBoolean("success");
-        String message = response.extract().jsonPath().getString("message");
-        assertEquals("Ожидается код ответа 401", SC_UNAUTHORIZED, statusCode);
-        assertEquals("Ожидается false", false, success);
-        assertEquals("Ожидается другое сообщение",
-                "email or password are incorrect", message);
+        response.assertThat()
+                .statusCode(SC_UNAUTHORIZED)
+                .body("success", equalTo(false))
+                .body("message", equalTo("email or password are incorrect"));
     }
 
     @Test
-    @DisplayName("POST /api/auth/login попытка входа под несуществующим пользователем с email null")
+    @DisplayName("POST /api/auth/login попытка входа под несуществующим " +
+            "пользователем с email null")
     @Description("Код 401 при передаче в body email null и корректного password")
     public void userLoginWithNullEmailAndCorrectPass401Test() {
-        credentials = new Credentials(null, "password");
+        credentials = new Credentials(null, genPass);
         response = client.loginUser(credentials);
 
-        int statusCode = response.extract().statusCode();
-        boolean success = response.extract().jsonPath().getBoolean("success");
-        String message = response.extract().jsonPath().getString("message");
-        assertEquals("Ожидается код ответа 401", SC_UNAUTHORIZED, statusCode);
-        assertEquals("Ожидается false", false, success);
-        assertEquals("Ожидается другое сообщение",
-                "email or password are incorrect", message);
+        response.assertThat()
+                .statusCode(SC_UNAUTHORIZED)
+                .body("success", equalTo(false))
+                .body("message", equalTo("email or password are incorrect"));
     }
 
     @Test
-    @DisplayName("POST /api/auth/login попытка входа под несуществующим пользователем с password null")
+    @DisplayName("POST /api/auth/login попытка входа под несуществующим " +
+            "пользователем с password null")
     @Description("Код 401 при передаче в body существующего email и password null")
     public void userLoginWithCorrectEmailAndPassNull401Test() {
         credentials = new Credentials(genEmail, null);
         response = client.loginUser(credentials);
 
-        int statusCode = response.extract().statusCode();
-        boolean success = response.extract().jsonPath().getBoolean("success");
-        String message = response.extract().jsonPath().getString("message");
-        assertEquals("Ожидается код ответа 401", SC_UNAUTHORIZED, statusCode);
-        assertEquals("Ожидается false", false, success);
-        assertEquals("Ожидается другое сообщение",
-                "email or password are incorrect", message);
+        response.assertThat()
+                .statusCode(SC_UNAUTHORIZED)
+                .body("success", equalTo(false))
+                .body("message", equalTo("email or password are incorrect"));
     }
     @After
     @DisplayName("DELETE /api/auth/user")
